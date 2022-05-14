773ea24ed8aacd71779b5a299aeb374742ede480
aust1inn <austinotieno364@gmail.com>
2022-05-14 10:16:15 +0300

    adds user_loader functionality

--- app/models.py
+++ app/models.py
@@ -3,6 +3,10 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import datetime
 from flask_login import UserMixin
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 #Model Class for User
 class User(db.Model,UserMixin):
 
