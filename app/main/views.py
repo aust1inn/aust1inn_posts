3727388178cb86628dce50cc4bacb1eb4d7447fd
aust1inn <austinotieno364@gmail.com>
2022-05-14 14:32:47 +0300

    sets up posts routes

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
 import secrets,os
 from .. import bcrypt,db
+from PIL import Image
 from flask import render_template,url_for,flash,redirect,request,abort
-from .forms import RegistrationForm,LoginForm,UpdateAccountForm,CommentForm
+from .forms import RegistrationForm,LoginForm,UpdateAccountForm,PostForm
 from ..models import User,Post,Comment
 from flask_login import login_user,current_user,logout_user,login_required
 
@@ -29,7 +30,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in' , 'success')
-        return redirect(url_for('login'))
+        return redirect(url_for('main.login'))
       
     return render_template('register.html' ,title='register' ,form=form)      
 
@@ -43,13 +44,107 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page= request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
+            return redirect(next_page) if next_page else redirect(url_for('main.index'))
         else:    
             flash("Login unsuccesful.Please check email and password",'danger')
 
-    return render_template('login.html' ,title='login', form=form)    
+    return render_template('login.html' ,title='login', form=form)        
 
 @main.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('home'))    
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(main.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    
+    return picture_fn
+
+@main.route('/account',methods=['GET','POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
+        current_user.username=form.username.data
+        current_user.email=form.email.data
+        db.session.commit()
+        flash('Your account has been updated', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+  
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html' ,title='account', image_file=image_file, form=form)  
+
+
+@main.route('/post/new',methods=['GET','POST'])
+@login_required
+def new_post(): 
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data , author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created','success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html' ,title='New Post',form=form,legend='New post')    
+
+@main.route('/post/<int:post_id>')
+@login_required
+def post(post_id):
+    post=Post.query.get_or_404(post_id)
+    return render_template('post.html',title=post.title,post=post) 
+
+@main.route('/post/<int:post_id>/update',methods=['GET','POST'])
+@login_required
+def update_post(post_id):
+    post=Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form=PostForm()
+    if form.validate_on_submit():
+        post.title=form.title.data
+        post.content=form.content.data
+        db.session.commit() 
+        flash("Your post has been updated","success")
+        return redirect(url_for('post',post_id=post.id))
+    elif request.method == 'GET':    
+        form.title.data=post.title
+        form.content.data=post.content   
+    return render_template('create_post.html' ,title='Update Post',
+                            form=form,legend='Update Post')    
+
+
+@main.route("/post/<int:post_id>/delete", methods=['POST','GET'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
+
+@main.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)          
--- app/models.py
+++ app/models.py
@@ -13,22 +13,22 @@ class User(db.Model, UserMixin):
     username=db.Column(db.String(20),unique=True,nullable=False)
     email=db.Column(db.String(120),unique=True,nullable=False)
     image_file=db.Column(db.String(20),nullable=False,default='default.jpg')
-    password=db.Column(db.String(60),nullable=False)
+    password=db.Column(db.String(60),nullable=True)
     posts=db.relationship('Post',backref='author',lazy=True)
     comment = db.relationship('Comment', backref='author', lazy='dynamic')
 
 
-    @property
-    def password(self):
-        raise AttributeError('You cannot read the password attribute')
+    # @property
+    # def password(self):
+    #     raise AttributeError('You cannot read the password attribute')
 
-    @password.setter
-    def password(self, password):
-        self.pass_secure = generate_password_hash(password)
+    # @password.setter
+    # def password(self, password):
+    #     self.pass_secure = generate_password_hash(password)
 
 
-    def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
+    # def verify_password(self,password):
+    #     return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
         return f'User {self.username}'
